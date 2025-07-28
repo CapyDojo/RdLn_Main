d58bfaa0aba9753fd00c50bfc1c1c6bf3c0cb5b6
CapyDojo <capydojo@gmail.com>
2025-07-29 05:06:41 +1000

    "20250729_Commit_B_Lothlorien_Theme_Refined"

--- src/themes/definitions/lothlorien.ts
+++ src/themes/definitions/lothlorien.ts
@@ -26,7 +26,7 @@ export const lothlorienTheme: ThemeConfig = {
             200: '#bbf7d0',
             300: '#86efac',
             400: '#4ade80',
-            500: '#22c55e',
+            500: '#0d7c47',
             600: '#16a34a',
             700: '#15803d',
             800: '#166534',
